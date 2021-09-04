84cf07ec3e18abc305df24db4821a2f8b0d2acf7
Bodhoditya <89837846+Bodhoditya@users.noreply.github.com>
2021-09-04 18:29:57 +0530

    Update lda two variables.R

--- lda two variables.R
+++ lda two variables.R
@@ -1,4 +1,4 @@
-##########################21(12)
+#######################12
 library(MASS)
 library(tidyverse)
 library(ggplot2)
@@ -19,15 +19,15 @@ x <- seq(min(datPred[,2]), max(datPred[,2]), length.out=150)
 y <- seq(min(datPred[,3]), max(datPred[,3]), length.out=150)
 Xcon <- matrix(c(rep(x,length(y)),
                  rep(y, rep(length(x), length(y)))),,2)
-r3.pr1 <- predict(r3, Xcon)$post[, c("setosa","versicolor")] %*% c(1,1) 
-r3.pr2 <- predict(r3, Xcon)$post[, c("virginica","setosa")] %*% c(1,1)
+r3.pr1 <- predict(r3, Xcon)$post[, c("setosa","virginica")] %*% c(1,1) 
+r3.pr2 <- predict(r3, Xcon)$post[, c("setosa","versicolor")] %*% c(1,1)
 pr3<-data.frame(x=rep(x, length(y)), y=rep(y, each=length(x)), 
                 z1=as.vector(r3.pr1), z2=as.vector(r3.pr2))
 ggplot(datPred, aes(x=LD1, y=LD2) ) + 
   geom_point(size = 2, aes(pch = Species,  col=Species)) +
-  geom_contour(data=pr3, aes(x=x, y=y, z=z1), breaks=c(0,.5)) + 
-  geom_contour(data=pr3, aes(x=x, y=y, z=z2), breaks=c(0,.5))+
-  labs(title="Classification Based on X1 and X2")
+  labs(title="Classification Based on X1 and X2")+
+  geom_vline(xintercept=1)+
+  geom_abline(intercept = -1.3, slope = -.9)
 ###################################### 22(13)
 ##########################
 library(MASS)
